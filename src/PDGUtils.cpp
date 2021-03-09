224c46de88bb65186e371bcab9a3f6e4dfa86a2a
Yongzhe Huang <jk.aristode@gmail.com>
2021-03-09 16:41:15 -0500

    fix dot print error on null ditype

--- src/PDGUtils.cpp
+++ src/PDGUtils.cpp
@@ -203,6 +203,8 @@ void pdg::pdgutils::printTreeNodesLabel(Node *node, raw_string_ostream &OS, std:
   TreeNode *n = static_cast<TreeNode *>(node);
   int tree_node_depth = n->getDepth();
   DIType *node_di_type = n->getDIType();
+  if (node_di_type == nullptr)
+    return;
   std::string field_type_name = dbgutils::getSourceLevelTypeName(*node_di_type);
   OS << tree_node_type_str << " | " << tree_node_depth << " | " << field_type_name;
 }
